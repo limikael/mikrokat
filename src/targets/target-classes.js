9a1bcf1c1b93ae0f69d5644a80d98772c956b8ef
Mikael Lindqvist <li.mikael@gmail.com>
2025-06-17 22:40:45 +0800

    bla

--- src/targets/target-classes.js
+++ src/targets/target-classes.js
@@ -1,11 +1,12 @@
 import CloudflareTarget from "./CloudflareTarget.js";
 import FastlyTarget from "./FastlyTarget.js";
 import VercelTarget from "./VercelTarget.js";
+import NetlifyTarget from "./NetlifyTarget.js";
 
 export default {
 	"cloudflare": CloudflareTarget,
 	"fastly": FastlyTarget,
 	"vercel": VercelTarget,
-	"netlify": null,
-	"render": null,
+	"netlify": NetlifyTarget,
+	//"render": null,
 };
