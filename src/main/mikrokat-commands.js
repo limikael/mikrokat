20161419c91abf1afcfeca595d31947d06aeaa79
Mikael Lindqvist <li.mikael@gmail.com>
2025-06-27 14:25:45 +0800

    don't walk up the tree on init

--- src/main/mikrokat-commands.js
+++ src/main/mikrokat-commands.js
@@ -1,10 +1,8 @@
 import MikrokatProject from "./MikrokatProject.js";
 import {getEffectiveCwd} from "../utils/node-util.js";
 
-async function initOptions(options, {allowUninitialized}={}) {
+async function initOptions(options) {
 	options={...options};
-
-	options.cwd=await getEffectiveCwd(options.cwd,{allowUninitialized});
 
 	if ((options.silent || options.quiet) && !options.log)
 		options.log=false;
@@ -13,14 +11,15 @@ async function initOptions(options, {allowUninitialized}={}) {
 }
 
 export async function mikrokatInit(options) {
-	options=await initOptions(options,{allowUninitialized: true});
+	options=await initOptions(options);
 	let project=new MikrokatProject(options);
 
 	await project.init();
 }
 
 export async function mikrokatServe(options) {
-	options=await initOptions(options,{allowUninitialized: true});
+	options.cwd=await getEffectiveCwd(options.cwd);
+	options=await initOptions(options);
 	let project=new MikrokatProject(options);
 	await project.load();
 	await project.serve();
@@ -33,7 +32,8 @@ export async function mikrokatServe(options) {
 }
 
 export async function mikrokatBuild(options) {
-	options=await initOptions(options,{allowUninitialized: true});
+	options.cwd=await getEffectiveCwd(options.cwd);
+	options=await initOptions(options);
 	let project=new MikrokatProject(options);
 	await project.load();
 
