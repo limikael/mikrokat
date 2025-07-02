e81978657af8e8149abee8095d4e9fdc05593f95
Mikael Lindqvist <li.mikael@gmail.com>
2025-07-02 14:35:47 +0800

    ver

--- src/exports/default.js
+++ src/exports/default.js
@@ -1,6 +1,6 @@
 export {default as MikrokatServer} from "../main/MikrokatServer.js";
 export {mikrokatBuild, mikrokatServe, mikrokatInit, 
 		mikrokatCreateProvisionEnv, mikrokatGetPlatforms, 
-		mikrokatClean} from "../main/mikrokat-commands.js";
+		mikrokatClean, mikrokatDeploy} from "../main/mikrokat-commands.js";
 export {processProjectFile} from "../utils/project-util.js";
 export {checkDependenciesUpToDate, assertDependenciesUpToDate} from "../utils/npm-util.js";
